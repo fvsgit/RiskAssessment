a575ff36c27d031f78db9154fc08f11edd2a93c2
fvsgit <francoisvans@gmail.com>
2017-11-06 19:02:42 +0000

    Removed files and code that conflicts with the visual studio solution. This is now ready to move to visual studio for platform compilation

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,9 +1,8 @@
 sap.ui.define([
 		"sap/ui/core/UIComponent",
 		"sap/ui/Device",
-		"riskassessment/model/models",
-		"riskassessment/controller/ErrorHandler"
-	], function (UIComponent, Device, models, ErrorHandler) {
+		"riskassessment/model/models"
+	], function (UIComponent, Device, models) {
 		"use strict";
 
 		return UIComponent.extend("riskassessment.Component", {
@@ -22,9 +21,6 @@ sap.ui.define([
 				// call the base component's init function
 				UIComponent.prototype.init.apply(this, arguments);
 
-				// initialize the error handler with the component
-				this._oErrorHandler = new ErrorHandler(this);
-
 				// set the device model
 				this.setModel(models.createDeviceModel(), "device");
 				
@@ -37,12 +33,10 @@ sap.ui.define([
 
 			/**
 			 * The component is destroyed by UI5 automatically.
-			 * In this method, the ErrorHandler is destroyed.
 			 * @public
 			 * @override
 			 */
 			destroy : function () {
-				this._oErrorHandler.destroy();
 				// call the base component's destroy function
 				UIComponent.prototype.destroy.apply(this, arguments);
 			},
